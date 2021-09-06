1c8e8b6b20f26ecb39e793aeb5f5ac8614910955
Shota Iwata <iwata.s1107@gmail.com>
2021-09-07 00:19:34 +0900

    Initial Commit

---
+++ Piano/PianoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PianoApp.swift
+//  Piano
+//
+//  Created by 岩田照太 on 2021/09/07.
+//
+
+import SwiftUI
+
+@main
+struct PianoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
