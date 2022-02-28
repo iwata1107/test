060e29662cc15ee22f0866c4ee2ccb1e7156d549
iwata1107 <76892054+iwata1107@users.noreply.github.com>
2022-02-28 19:09:25 +0900

    Add files via upload

---
+++ FirebaseLoginSample/FirebaseLoginSample/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  FirebaseLoginSample
+//
+//  Created by 岩田照太 on 2021/08/18.
+//
+import SwiftUI
+import Firebase
+
+struct ContentView: View {
+    @State var email = ""
+    @State var password = ""
+
+    var body: some View {
+        VStack {
+            TextField("Email", text: $email)
+            SecureField("Password", text: $password)
+            Button(action: { login() }) {
+                Text("Sign in")
+            }
+        }
+        .padding()
+    }
+
+    func login() {
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            if error != nil {
+                print(error?.localizedDescription ?? "")
+            } else {
+                print("success")
+            }
+        }
+    }
+}
